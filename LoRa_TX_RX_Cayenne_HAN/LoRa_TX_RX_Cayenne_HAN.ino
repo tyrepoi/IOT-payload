8a60a32ba9b88399f08a2e33d14e88f09b9c1a27
LenVerploegen <ljb.verploegen@student.han.nl>
2024-03-22 16:22:53 +0100

    removed lines

--- LoRa_TX_RX_Cayenne_HAN/LoRa_TX_RX_Cayenne_HAN.ino
+++ LoRa_TX_RX_Cayenne_HAN/LoRa_TX_RX_Cayenne_HAN.ino
@@ -289,16 +289,14 @@ void loop(){
 
   float custom = 300.1;
 
-  lpp.addTemperature(LPP_CH_TEMPERATURE, temperature);
+  //lpp.addTemperature(LPP_CH_TEMPERATURE, temperature);
   //lpp.add2Bytes(LPP_CH_TEMPERATURE,LPP_TEMPERATURE, temperature, 10);
   lpp.addCustomByte(LPP_CH_CUSTOMBYTE, LPP_CUSTOMBYTE, custom, 10, 2, 1);
-
 
   lpp.addWord(LPP_CH_TEMPERATURE, LPP_TEMPERATURE, temperature, 10);
   lpp.addByte(LPP_CH_HUMIDITY, LPP_RELATIVE_HUMIDITY, humidity, 2);
   lpp.addWord(LPP_CH_LUMINOSITY, LPP_LUMINOSITY, luminosity, 1);
   lpp.addByte(LPP_CH_ROTARYSWITCH, LPP_DIGITAL_INPUT, rotaryPosition, 1);
-  //lpp.addAccelerometer(LPP_CH_ACCELEROMETER, x, y, z);
   lpp.add3Float(LPP_CH_ACCELEROMETER, LPP_ACCELEROMETER, x, y, z, 1000);
   lpp.addWord(LPP_CH_BOARDVCCVOLTAGE, LPP_ANALOG_INPUT, vdd, 100);
   lpp.addByte(LPP_CH_PRESENCE, LPP_PRESENCE, SAFE, 1);
